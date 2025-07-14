ba0d89f47b4c9458ae1b284f884b54bf9b61d6eb
Ragesploit-x <hhaghooo+1@gmail.com>
2025-07-14 17:13:03 +0300

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,6 +1,7 @@
 function GoTo(Location)
     if Location == "Home" then
         print("Going home!")
+        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-2985.95337, 3993, -9006.82324)
         game:GetService("ReplicatedStorage").API:FindFirstChild("LocationAPI/SetLocation"):FireServer("housing", LocalPlayer)
         for _, Furniture in pairs(OwnedFurnitures) do
             game:GetService("ReplicatedStorage").API:FindFirstChild("DownloadsAPI/Download"):InvokeServer("Furniture", Furniture)
