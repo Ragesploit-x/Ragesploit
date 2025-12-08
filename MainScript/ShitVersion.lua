e1853b83e964568e5e109d01358b69131f504e1e
Ragesploit-x <hhaghooo+1@gmail.com>
2025-12-08 10:57:04 +0300

    Update notification title from 'Really old' to 'Outdated'

--- MainScript/ShitVersion.lua
+++ MainScript/ShitVersion.lua
@@ -1,5 +1,5 @@
 game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "🕸️ Really old script...",
+    Title = "🕸️ Outdated script...",
     Text = "Please join our discord for the latest update.\nLink: discord.gg/ragesploit",
     Duration = 60
 })
