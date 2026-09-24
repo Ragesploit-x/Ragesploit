6bed1ae9762236eae11f0e897cc529d2e6c72bcb
Ragesploit-x <hhaghooo+1@gmail.com>
2026-09-24 20:26:57 +0300

    Simplify request function initialization
    Refactor request function to simplify loading method.

--- UI.lua
+++ UI.lua
@@ -158,7 +158,7 @@ local useStudio = RunService:IsStudio() or false
 local settingsCreated = false
 local settingsInitialized = false -- Whether the UI elements in the settings page have been set to the proper values
 local prompt = useStudio and require(script.Parent.prompt) or loadWithTimeout('https://raw.githubusercontent.com/SiriusSoftwareLtd/Sirius/refs/heads/request/prompt.lua')
-local requestFunc = (syn and syn.request) or (fluxus and fluxus.request) or request or http_request or (http and http.request)
+local requestFunc = request or http_request or (http and http.request)
 
 -- Validate prompt loaded correctly
 if not prompt and not useStudio then
